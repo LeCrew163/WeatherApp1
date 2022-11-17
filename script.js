f52d86bd119e038c9f73e9a4c803d54002d255c9
Nikolai Martyn <64727475+LeCrew163@users.noreply.github.com>
2022-11-17 12:57:38 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+// key = a2dd9b02ab4631b036e5241ab1440e4e
+// url = https://api.openweathermap.org/data/2.5/weather
+
+const appId = "a2dd9b02ab4631b036e5241ab1440e4e";
+const appUrl = "https://api.openweathermap.org/data/2.5/weather";
+
+function getData(city) {
+    url = appUrl + `?q=${city}&appid=${appId}&units=metric`;
+    request = new XMLHttpRequest();
+    request.open("GET", url, false)
+    request.send(null)
+    return JSON.parse(request.responseText)
+}
+
+function onSubmit() {
+    var text = document.getElementById('input').value
+    weather = getData(text)
+    console.log(weather)
+
+
+
+    switch (weather.weather[0].main) {
+        case "Clear":
+            document.getElementById('html').style.background = "linear-gradient(to bottom, #00f, #007)"; break;
+        case "Clouds":
+            document.getElementById('html').style.background = "linear-gradient(to bottom, #aaa, #007)"; break;
+        case "Mist":
+            document.getElementById('html').style.background = "linear-gradient(to bottom, #E5E4E2, #d9dbda)"; break;
+    }
+
+    updateData(weather);
+
+}
+
+function updateData(data) {
+    document.getElementById('city').textContent = weather.name;
+    document.getElementById('temp').textContent = `Temperature: ${Math.round(weather.main.temp)} °C`;
+}
+
+document.getElementById('btn').onclick = onSubmit;
+document.addEventListener('keyup', event => {
+    if (event.key === 'Enter') {
+        onSubmit();
+    }
+});
